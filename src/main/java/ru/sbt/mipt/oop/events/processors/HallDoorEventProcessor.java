fbe14f2a949e05305458f866def6dd5118059e3c
Совков Андрей Дмитриевич <Sovkov.A.D@sberbank.ru>
2020-10-27 22:19:57 +0300

    command fix

--- src/main/java/ru/sbt/mipt/oop/events/processors/HallDoorEventProcessor.java
+++ src/main/java/ru/sbt/mipt/oop/events/processors/HallDoorEventProcessor.java
@@ -1,6 +1,7 @@
 package ru.sbt.mipt.oop.events.processors;
 
 import ru.sbt.mipt.oop.commands.CommandType;
+import ru.sbt.mipt.oop.commands.SensorCommand;
 import ru.sbt.mipt.oop.commands.SimpleSensorCommand;
 import ru.sbt.mipt.oop.elements.*;
 import ru.sbt.mipt.oop.events.Event;
@@ -15,7 +16,11 @@ public class HallDoorEventProcessor implements EventProcessor {
         Event getHallDoor = new GetHallDoorEvent();
         smartHome.apply(getHallDoor, c -> {}); //Получаем айди двери в холле
         if (event.getType() == DOOR_CLOSED && event.getObjectId().equals(getHallDoor.getObjectId())) {
-            smartHome.apply(new HallDoorEvent(EventType.LIGHTS_OFF, event.getObjectId()), ((HomeComponent c) -> ((Light)c).setOn(false)));
+            smartHome.apply(new HallDoorEvent(EventType.LIGHTS_OFF, event.getObjectId()), ((HomeComponent c) -> {
+                ((Light)c).setOn(false);
+                SensorCommand command = new SimpleSensorCommand(CommandType.LIGHT_OFF, c.getId());
+                command.sendCommand();
+            }));
         }
     }
 }
